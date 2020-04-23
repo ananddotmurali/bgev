a324012f5bd3c1d924d71d5188dc4586ba4a73ef
Vijay <vijay10pk@gmail.com>
2020-04-23 18:02:46 +0530

    changes in cp owner registeration

--- src/app/bgev-cp-owner/bgev-cp-owner-component.ts
+++ src/app/bgev-cp-owner/bgev-cp-owner-component.ts
@@ -9,7 +9,18 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class BgCpOwnerComponent implements OnInit {
     loadComplete = false;
+    
     constructor(private router: Router, private _snackBar: MatSnackBar) {}
+    amenities= [{"icon_name":"Store","icon":"shopping_cart"},
+    {"icon_name":"Cafe","icon":"local_cafe"},
+    {"icon_name":"Hospital","icon":"local_hospital"},
+    {"icon_name":"Restaurant","icon":"local_dining"},
+    {"icon_name":"Parking","icon":"local_parking"},
+    {"icon_name":"Rest Room","icon":"wc"},
+    {"icon_name":"Children Area","icon":"child_care"},
+    {"icon_name":"Car Wash","icon":"local_car_wash"},
+    {"icon_name":"Car Service","icon":"build"},
+    {"icon_name":"ATM","icon":"atm"}];
     registeredUser() {
         this._snackBar.open('Registered Successfully. Please Login to Continue', '', {
             duration: 3000
